138793abaf0fff8681325701542420bb62d788aa
abdulla-ahmed <83808860+abdulla-ahmed@users.noreply.github.com>
2025-01-03 08:56:05 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PrescriptionForm from './components/PrescriptionForm';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <PrescriptionForm />
+    </div>
+  );
+}
+
+export default App;
